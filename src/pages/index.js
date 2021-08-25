f8e4cbc5de77a8a369f674a42897d40d391ea1fc
LSAA1234 <luyao2014@gmail.com>
2021-08-25 14:59:47 +0200

    Add image to Site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from '../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home">
 			   <p>
-			   		I'm making this by following the Gatsby Tutorial.
+			   		I'm making this by following the Gatsby Tutorial.					   
 			   </p>
+			   <StaticImage src="https://wallpapercave.com/wp/wp2649679.jpg" 
+					   		alt="detective conan"/>
+				<StaticImage src="../images/gugu.jfif" 
+					   		alt="gugu"/>
 		</Layout>
 	);
 };
