c521fe72d67b632070588d559551537d07d227fc
LSAA1234 <luyao2014@gmail.com>
2021-08-29 15:38:36 +0200

    add blogs to blogs page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
     return (
-        <Layout pageTitle="My Blog Posts">
-           <ul>
+        <Layout pageTitle="My Blog Posts">    
                {
-                   data.allFile.nodes.map(node => (
-                       <li key={node.name}>{node.name}</li>
+                   data.allMdx.nodes.map(node => (
+                       <article key={node.id}>
+                           <h2>{node.frontmatter.title}</h2>
+                           <p>Posted: {node.frontmatter.datePublished}</p>
+                           <MDXRenderer>
+                               {node.body}
+                           </MDXRenderer>
+                       </article>
                    )) 
                 }
-           </ul>
         </Layout>
     )
 }
@@ -19,11 +24,18 @@ const BlogPage = ({data}) => {
 export default BlogPage
 
 export const query = graphql`
-    query BlogName {
-        allFile {
+    query BlogQuery {
+        allMdx(sort: {fields: frontmatter___datePublished, order: DESC}) {
             nodes {
-                name
+                frontmatter {
+                    datePublished(formatString: "MMMM D, YYYY")
+                    slug
+                    source
+                    title
+                }
+                id
+                body
             }
         }
-    } 
+  }
 `
